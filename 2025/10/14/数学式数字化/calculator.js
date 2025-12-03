4723f9350fc6ab7654073d82a77c92f38c2a5bea
brightendavid <2511401105@qq.com>
2025-12-03 14:55:14 +0800

    Site updated: 2025-12-03 14:55:04

--- 2025/10/14/数学式数字化/calculator.js
+++
@@ -1,8 +0,0 @@
-function calculate() {
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let sum = num1 + num2;
-    document.getElementById('result').innerHTML = `两个数之和为 ${sum}`;
-}
-
-
